e15b1c56214d86d2fa957c350bc62ad049a1f083
khan4lov <aayankhan31101995@gmail.com>
2026-08-19 12:32:51 +0530

    update ssh_pull.cjs

--- ssh_pull.cjs
+++ ssh_pull.cjs
@@ -3,15 +3,32 @@ const { Client } = require('ssh2');
 const conn = new Client();
 conn.on('ready', () => {
   console.log('Client :: ready');
-  conn.exec('cd /www/wwwroot/invoice-portal-temp && npm install && npm run build', (err, stream) => {
+  const cmd = `
+    echo "Deploying invoice-portal-temp..."
+    cd /www/wwwroot/invoice-portal-temp
+    git pull
+    npm run build
+
+    echo "Deploying attendance_portal..."
+    cd /www/wwwroot/attendance_portal
+    git pull
+    npm run build
+
+    echo "Deploying whatsapp-backend..."
+    cd /www/wwwroot/whatsapp-backend
+    git pull
+    npm run build
+  `;
+  
+  conn.exec(cmd, (err, stream) => {
     if (err) throw err;
     stream.on('close', (code, signal) => {
       console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
       conn.end();
     }).on('data', (data) => {
-      console.log('STDOUT: ' + data);
+      process.stdout.write(data);
     }).stderr.on('data', (data) => {
-      console.log('STDERR: ' + data);
+      process.stderr.write(data);
     });
   });
 }).connect({
